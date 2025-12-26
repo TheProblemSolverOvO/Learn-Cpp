60f80e59cc74300f73f78420682ecc9bada8617d
Enoch Ohazurike <rhyme@Enochs-Mac.local>
2025-12-25 18:19:33 -0700

    More learning

---
+++ chapter1/literalsAndOperators.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <unordered_set>
+#include <stack>
+#include <tuple>
+#include <cstring>
+#include <map>
+#include <unordered_map>
+#include <numeric>
+#include <string>
+#include <cctype>
+
+using namespace std;
+#define fast_io ios::sync_with_stdio(false); cin.tie(0);
+typedef long long ll; 
+
+// use s.substr(1, 6) to start at position 1 and take 6 characters
+//use stoi to convert from string to int you dummy. 
+//use isupper, isdigit...they both work on char not str. 
+
+//for(const auto& x : mp) cout << x.first << " " << x.second << endl;
+//int minimum = min({a, b, c});
+//use gcd(a, b) == __gcd(a, b); from the algorithm
+
+//string stuff 
+//// Use transform(s.begin(), s.end(), s.begin(), ::tolower); to lowercase a string
+// Use transform(s.begin(), s.end(), s.begin(), ::toupper); to uppercase a string
+void solve(){
+
+}
+//What are hello world and 5 in the below code? they are Literals.  A literal(also known as a literal constant)
+//is a fixed value that has been inserted directly into the source code
+
+
+int main(){
+	fast_io;
+
+	cout << "Hello World" << "\n";//print the value of a literal
+	cout << 5 << "\n"; //print the value of a literal
+					   
+	int x{5};
+
+	cout << (x==5)<< "\n"; //print the value of a variable x;
+
+	return 0;
+}
+
+
